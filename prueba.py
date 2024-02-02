4a8d1ebb032b8f9e29bf4903bbcb8888de5f1263
dakardu <unodago@gmail.com>
2024-02-02 13:11:18 +0100

    Primer commit de la rama feacture1

---
+++ prueba.py
@@ -0,0 +1,6 @@
+def saludo(name, nikname):
+    print("Bienvenid@ " + name + ", su usuario en GitHub es " + nikname)
+
+name = input("Ingrese su nombre: ")
+nikname = input("Ingrese su nombre de usuario en GitHub: ")
+saludo(name, nikname)
