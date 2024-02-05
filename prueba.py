25732e3045a466db21a12408782f2822d6223011
carsanven <158622958+carsanven@users.noreply.github.com>
2024-02-05 19:59:33 +0100

    Update prueba.py lista meses

--- prueba.py
+++ prueba.py
@@ -4,3 +4,6 @@ def saludo(name, nikname):
 name = input("Ingrese su nombre: ")
 nikname = input("Ingrese su nombre de usuario en GitHub: ")
 saludo(name, nikname)
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+for mes in meses:
+    print(mes)
